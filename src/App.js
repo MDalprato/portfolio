3519504601e6bcea00f37e7429b1a5541c9d49b9
Marco Dalprato <marcodalprato@me.com>
2021-02-27 11:48:27 +0100

    cInitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+ 
+import React from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import AboutMe from "./AboutMe";
+import Contact from "./Contact";
 
+import './App.css';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+        <div>
+          <h1>Marco Dalprato</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/aboutme">About me</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/aboutme" component={AboutMe}/>
+            <Route path="/contact" component={Contact}/>
+          </div>             
+        </div>
+      </HashRouter>
   );
 }
-
+ 
 export default App;
